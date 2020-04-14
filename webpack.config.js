7c00cd7156a8f9cfd37c4ebe3ae870b947d360b6
naoya00 <bsk470@gmail.com>
2020-04-14 22:07:45 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var publidDir = __dirname + '/public';
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: publidDir,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: publidDir
+  }
+};
